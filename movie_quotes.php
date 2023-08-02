fb271bf883301ed91ccb1b0fc2e5f6efd1f86e38
Sherzad Ghairat <123905951+MadDeblin@users.noreply.github.com>
2023-08-02 14:36:54 +0200

    Funktion quotes
    Jag har lagt in koden för funktionen som gör random quotes

---
+++ movie_quotes.php
@@ -0,0 +1,9 @@
+<?php
+$quotes = [
+    "My mama always said life was like a box of chocolates. You never know what you're gonna get. - Forrest Gump",
+    "May the Force be with you. - Star Wars",
+    "I'll be back. - The Terminator",
+    "There's no place like home. - The Wizard of Oz",
+    // Add more quotes here
+];
+?>
