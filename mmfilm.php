34943bd766bac1d0f8840769cf7fbf8e783ae39d
Sherzad Ghairat <123905951+MadDeblin@users.noreply.github.com>
2023-08-03 17:38:16 +0200

    Update mmfilm.php

--- mmfilm.php
+++ mmfilm.php
@@ -26,16 +26,7 @@
             <a class="nav-link" href="#Header">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#Quotes">Quotes</a>
-          </li>
-          <li class="nav-item">
             <a class="nav-link" href="#Movies">Movies</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#Comments">Comments</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#Random">Random</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#Contact">Contact</a>
@@ -53,6 +44,34 @@
       </div>
     </div>
   </header>
+  <div class="quotes" id="quotes">
+		<!-- Random quotes från filmer funktion -->
+    <h3 id="quote-container">
+        <?php
+        include 'movie_quotes.php';
+        $randomIndex = array_rand($quotes);
+        echo $quotes[$randomIndex];
+        ?>
+    </h3>
+
+    <script>
+        function updateQuote() {
+            // AJAX request to get a new quote from the server
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("quote-container").innerText = this.responseText;
+                }
+            };
+            xhttp.open("GET", "movie_quotes_ajax.php", true);
+            xhttp.send();
+        }
+
+        // Update the quote every 5 seconds (5000 milliseconds)
+        setInterval(updateQuote, 3000);
+    </script>
+		
+	</div>
     
     
     <!---Titel Div -->
