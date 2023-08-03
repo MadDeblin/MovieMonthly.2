0ee28e29cea2e139d334bb2b47b8dfcbc0f48831
sanaalshihani <128450571+sanaalshihani@users.noreply.github.com>
2023-08-03 17:02:28 +0200

    Update mmfilm.php
    fixade en bugg med git

--- mmfilm.php
+++ mmfilm.php
@@ -44,6 +44,34 @@
       </div>
     </div>
   </header>
+  <div class="quotes" id="quotes">
+		<!-- Random quotes från filmer funktion -->
+    <h3 id="quote-container">
+        <?php
+        include 'movie_quotes.php';
+        $randomIndex = array_rand($quotes);
+        echo $quotes[$randomIndex];
+        ?>
+    </h3>
+
+    <script>
+        function updateQuote() {
+            // AJAX request to get a new quote from the server
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("quote-container").innerText = this.responseText;
+                }
+            };
+            xhttp.open("GET", "movie_quotes_ajax.php", true);
+            xhttp.send();
+        }
+
+        // Update the quote every 5 seconds (5000 milliseconds)
+        setInterval(updateQuote, 3000);
+    </script>
+		
+	</div>
     
     
     <!---Titel Div -->
