0ee5a6308743ed04ccb2b3241c44afe10a0fbcfb
MadDeblin <123905951+MadDeblin@users.noreply.github.com>
2023-08-03 16:02:18 +0200

    Update movie_quotes_ajax.php
    commited random quote changes

--- movie_quotes_ajax.php
+++ movie_quotes_ajax.php
@@ -7,7 +7,29 @@ $quotes = [
     // Add more quotes here
 ];
 
+// Function to format the quote with proper punctuation and attribution
+function formatQuote($quote) {
+    // Remove any trailing spaces and periods from the quote
+    $quote = rtrim($quote, ' .');
+
+    // Check if the quote has an attribution (indicated by a hyphen)
+    if (!preg_match('/-/', $quote)) {
+        $quote .= " - Unknown"; // You can replace "Unknown" with the default attribution you want to use
+    }
+
+    // Add a period at the end of the quote if it's missing
+    if (!preg_match('/\.$/', $quote)) {
+        $quote .= '.';
+    }
+
+    return $quote;
+}
+
 $randomIndex = array_rand($quotes);
-echo $quotes[$randomIndex];
-?>
+$randomQuote = $quotes[$randomIndex];
+
+// Format the quote with proper punctuation and attribution
+$formattedQuote = formatQuote($randomQuote);
+
+echo $formattedQuote;
 ?>
