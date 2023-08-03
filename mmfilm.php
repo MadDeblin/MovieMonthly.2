d35da5948a060c55d1978f6bc27bda4f801c3ab1
MadDeblin <123905951+MadDeblin@users.noreply.github.com>
2023-08-03 17:15:42 +0200

    Update mmfilm.php

--- mmfilm.php
+++ mmfilm.php
@@ -44,7 +44,8 @@
       </div>
     </div>
   </header>
-  <div class="quotes" id="quotes">
+
+  <div class="Quote" id="Quote">
 		<!-- Random quotes från filmer funktion -->
     <h3 id="quote-container">
         <?php
@@ -165,6 +166,7 @@ Join the MovieMonthly community today and never miss out on the latest and great
   </div>
 </section>
         </div>
+        <div class="Comment" id="Comment">
       <h3>Share your thoughts on this months movies! </h3>
          <form action="add_comment.php" method="POST">
           <label for="name">Name:</label>
@@ -209,6 +211,26 @@ Join the MovieMonthly community today and never miss out on the latest and great
     <input type="submit" value="Submit">
   </form>
 </div>
+<div class="Random" id="Random">
+<h1>Har du svårt att välja en film?</h1>
+    <p>Tryck på knappen så slumpas en film mellan våra 5 filmrekommenatdtioner.</p>
+    <form method="POST">
+        <input type="submit" name="slumpa" value="Slumpa nummer">
+    </form>
+    <p>
+        <?php
+        function slumpmässigt_nummer_1_5() {
+            return rand(1, 5);
+        }
+
+        if (isset($_POST['slumpa'])) {
+            // Anropar funktionen för slumpat nummer mellan 1 och 5
+            $slumpNummer = slumpmässigt_nummer_1_5();
+            echo "Kolla på filmrekommendation nummer: " . $slumpNummer;
+        }
+        ?>
+    </p>
+  </div>
   <!-- Footer -->
   <footer class="container-fluid">
     <div class="row">
